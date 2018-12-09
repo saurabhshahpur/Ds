938a11424aaf7911c3d5b3ec420dfecffa6ff24f
Saurabh Singh <saurabhshahpur@gmail.com>
2018-12-09 18:37:28 +0530

    added

--- Graph/Vertex.py
+++ Graph/Vertex.py
@@ -1,16 +1,16 @@
 class Vertex(object):
-    def __init__(self, key):
+    def __init__(self, key):  # O(1)
         self.id = key
         self.connected_to = {}
 
-    def addNeighbor(self, nbr, w=0):
+    def addNeighbor(self, nbr, w=0):  # O(1)
         self.connected_to[nbr] = w
 
-    def getWeight(self, nbr):
+    def getWeight(self, nbr):  # O(1)
         return self.connected_to[nbr]
 
-    def getId(self):
+    def getId(self):  # O(1)
         return self.id
 
-    def getConnections(self):
+    def getConnections(self):   # O(n) where n is the number of node which are connected to this
         return self.connected_to.keys()
