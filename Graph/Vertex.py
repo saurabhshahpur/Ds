1909f61a00ac5fc9ac4311e54625b2bafe2e3c82
Saurabh Singh <saurabhshahpur@gmail.com>
2018-12-09 18:35:14 +0530

    added vertex functions

--- Graph/Vertex.py
+++ Graph/Vertex.py
@@ -1,3 +1,16 @@
 class Vertex(object):
-    def __init__(self):
-        pass
+    def __init__(self, key):
+        self.id = key
+        self.connected_to = {}
+
+    def addNeighbor(self, nbr, w=0):
+        self.connected_to[nbr] = w
+
+    def getWeight(self, nbr):
+        return self.connected_to[nbr]
+
+    def getId(self):
+        return self.id
+
+    def getConnections(self):
+        return self.connected_to.keys()
