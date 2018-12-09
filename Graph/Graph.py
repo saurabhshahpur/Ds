994fc3d7e3763e1ea20d31f4c790d0f8b6905baf
Saurabh Singh <saurabhshahpur@gmail.com>
2018-12-09 18:52:54 +0530

    added graph funcitions

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -1,3 +1,39 @@
+from Vertex import Vertex
+
+
 class Graph(object):
     def __init__(self):
-        pass
+        self.no_of_vertex = 0
+        self.no_of_edge = 0
+        self.vertices = {}
+
+    def addVertex(self, u):
+        new_vertex = Vertex(u)
+        self.no_of_vertex += 1
+        self.vertices[u] = new_vertex
+        return new_vertex
+
+    def getVertex(self, u):
+        if u in self.vertices:
+            return self.vertices[u]
+        return None
+
+    def addEdge(self, u, v, w=0):
+        if u not in self.vertices:
+            # create new vertex
+            self.addVertex(u)
+        if v not in self.vertices:
+            self.addVertex(v)
+        self.no_of_edge += 1
+        self.vertices[u].addNeighbor(self.vertices[v], w)
+
+    def getVertices(self):
+        return self.vertices.keys()
+
+    # |V|
+    def getVertexSize(self):
+        return self.no_of_vertex
+
+    # |E|
+    def getEdgeSize(self):
+        return self.no_of_edge
